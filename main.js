c51e5f96c88431b65f5f7dcfca42782bd7f25c1a
GastlyVirus <gstnedgrdrch93@gmail.com>
2023-03-21 18:55:14 -0300

    primera subida

---
+++ main.js
@@ -0,0 +1,12 @@
+// Creo las constantes para poder mostrar la fecha actual, obtener el value del input, agregar a un array el value del input y mostrar los value guardados.
+const fecha = document.getElementById('fecha');
+const input = document.getElementById('input');
+const botonAgregar = document.getElementById('botonAgregar');
+const lista = document.getElementById('lista');
+
+
+
+//
+function agregarTarea(tarea){
+    
+}
